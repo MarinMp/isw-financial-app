d76937cba3771bb5652400b8deab8c18efca8b2a
MarinMp <mpmarins@unbosque.edu.co>
2026-05-23 22:22:51 -0500

    PM-ISW1: Commit inicial proyecto Spring Boot

---
+++ backend/src/test/java/co/edu/unbosque/gestion_financiera/GestionFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.gestion_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
